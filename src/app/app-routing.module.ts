3e5c243ade05edb0b0ccaee43a4d407d59104d6c
PabloNValdez <pablovaldez0301@gmail.com>
2022-09-14 09:05:11 -0300

    Finalización configuración básica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MaintenanceComponent } from './shared/maintenance/maintenance.component';
+import { NotFound404Component } from './shared/not-found404/not-found404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'maintenance', component: MaintenanceComponent },
+  { path: '**', component: NotFound404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MaintenanceComponent } from './shared/maintenance/maintenance.component';
+import { NotFound404Component } from './shared/not-found404/not-found404.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MaintenanceComponent,
+    NotFound404Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
